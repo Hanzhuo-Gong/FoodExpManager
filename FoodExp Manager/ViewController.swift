17350ab060e7eca8fd61d26a5d9dda90b6355255
Hanzhuo-Gong <hgong@scu.edu>
2022-10-16 21:30:58 -0700

    Initial Commit

---
+++ FoodExp Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodExp Manager
+//
+//  Created by Hanzhuo Gong on 10/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
