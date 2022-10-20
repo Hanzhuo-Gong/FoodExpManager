70d314bda8787342aca8e97e8b1df513b0b25e4f
Hanzhuo-Gong <hgong@scu.edu>
2022-10-19 22:13:21 -0700

    Food page functionality update

--- FoodExp Manager/Controller/ItemListViewController.swift
+++ FoodExp Manager/Controller/ItemListViewController.swift
@@ -6,37 +6,119 @@
 //
 
 import UIKit
+import CoreData
 
 class ItemListViewController: UITableViewController {
-
-    let testArray = ["Test1", "Test2", "Test3"]
+    
+    var foodArray = [Food]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         tableView.rowHeight = 80.0
+        loadItems()
     }
 
     //MARK - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testArray.count
+        return foodArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        
-        cell.textLabel?.text = testArray[indexPath.row]
+        cell.textLabel?.text = foodArray[indexPath.row].name
         
         return cell
     }
     
     //MARK - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+        
+        //Delete item for testing
+        context.delete(foodArray[indexPath.row])
+        foodArray.remove(at: indexPath.row)
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        //print("add button presseed")
+        
+        //TODO - Create a New UI for the food page, and place the function in there
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Food", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newFood = Food(context: self.context)
+            newFood.name = textField.text
+            self.foodArray.append(newFood)
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new food"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //Update CoreData value
+    //foodArray[indexPath.row].setValue("Completed", forKey: "title")
+    
+    
+    //MARK - Model Manupulation Methods
+    func saveItems() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Food> = Food.fetchRequest()) {
+        
+        do {
+            foodArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
 }
 
+//MARK: - Search bar methods
+extension ItemListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //TODO: change to firebase fetchrequest later
+        let request : NSFetchRequest<Food> = Food.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
