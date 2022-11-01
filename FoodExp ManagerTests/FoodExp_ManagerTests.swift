7a41ad742a6481d8bc5b0a02df7b89fb4b07c625
Hanzhuo-Gong <hgong@scu.edu>
2022-10-31 22:29:50 -0700

    add unit testing & validation service

---
+++ FoodExp ManagerTests/FoodExp_ManagerTests.swift
@@ -0,0 +1,15 @@
+//
+//  FoodExp_ManagerTests.swift
+//  FoodExp ManagerTests
+//
+//  Created by Hanzhuo Gong on 10/31/22.
+//
+
+import XCTest
+
+
+final class FoodExp_ManagerTests: XCTestCase {
+
+    
+
+}
